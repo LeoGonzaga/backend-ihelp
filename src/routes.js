97f47e3c37dc4ebda6c15f8ac0f52df666dcfc99
Leonardo Gonzaga <sacredlotus05@gmail.com>
2020-06-22 21:53:16 -0300

    passando a rota certa pro arquivo de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
-const { Router } = require("express");
-const routers = new Router();
-// const routes = express.Router();
-
+const express = require("express");
+const routers = express.Router();
 
 const SessionController = require("./controllers/SessionController");
 const UserController = require("./controllers/UserController");
@@ -14,7 +12,7 @@ routers.post("/createAccount", UserController.createUser);
 routers.post("/login", SessionController.login);
 routers.post("/reset", UserController.recoveryPassword);
 routers.get("/user", auth.auth, UserController.viewUser);
-routers.put("/user/update", auth.auth, UserController.updateUser)
+routers.put("/user/update", auth.auth, UserController.updateUser);
 routers.get("/feed", auth.auth, PostController.viewPosts);
 routers.post("/feed/create", auth.auth, PostController.createPost);
 routers.delete("/feed/delete", auth.auth, PostController.deletePost);

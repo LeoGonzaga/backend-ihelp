814326ee53ae4c0224207b1f6f8316dc6e8d3251
Leonardo Gonzaga <sacredlotus05@gmail.com>
2020-06-29 21:21:33 -0300

    removendo token de perfil

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ const auth = require("./middlewares/auth");
 routers.post("/createAccount", UserController.createUser);
 routers.post("/login", SessionController.login);
 routers.post("/reset", UserController.recoveryPassword);
-routers.get("/user", auth.auth, UserController.viewUser);
+routers.get("/user", UserController.viewUser);
 routers.put("/user/update", auth.auth, UserController.updateUser);
 routers.get("/feed", PostController.viewPosts);
 routers.post("/feed/create", auth.auth, PostController.createPost);
